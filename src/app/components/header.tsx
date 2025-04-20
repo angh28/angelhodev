5ec4f79ec2e24f2ead63e827fec7fef3ff339622
angel <ah.angelho@gmail.com>
2025-04-20 15:53:20 +1000

    Add email functionality, mobile responsiveness

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,14 @@
+import {HeartIcon} from '@heroicons/react/24/solid'
+
+
 export default function Header() {
     return (
-        <nav className="sticky top-0 flex items-center justify-start p-5 pl-50 gap-10">
-        <div className="font-extrabold">angel</div>
-        <button >home</button>
-        <button>about</button>
-        <button>contact</button>
+    <nav className="sticky top-0 flex items-center justify-start p-5 pl-5 md:pl-50 gap-10 bg-white">
+        <div className="font-extrabold text-xl">angel</div>
+        <button className="cursor-pointer hover:scale-110">home</button>
+        <button className="cursor-pointer hover:scale-110">about</button>
+        <button className="cursor-pointer hover:scale-110">contact</button>
+        <HeartIcon className="size-5 text-sky-400" />
       </nav>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+'use client'
 import Header from "./components/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <div className="flex flex-col pl-50 mt-50 gap-5">
-        <h3 className="font-bold text-3xl mb-2 text-indigo-400">Hi! I&apos;m Angel</h3>
-        <button className="w-1/7 font-bold rounded bg-sky-300 hover:bg-sky-500 text-white py-2 px-4 ">Contact Me</button>
+      <div className="flex flex-col pl-5 md:pl-50 mt-50 gap-5">
+        <div className="flex flex-col">
+          <h3 className="font-bold text-3xl mb-2 text-indigo-400">
+            Hi! I&apos;m Angel
+          </h3>
+        </div>
+        <button
+          className="w-45 cursor-pointer font-bold rounded bg-sky-500 hover:bg-sky-700 text-white py-2 px-4 "
+          onClick={() => (window.location.href = "mailto:ah.angelho@gmail.com")}
+        >
+          Contact Me
+        </button>
       </div>
     </div>
   );
