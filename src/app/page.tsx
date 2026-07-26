b911f44d6bf53198bb7115b24c175b319fbaadbe
angel <ah.angelho@gmail.com>
2026-07-26 22:49:01 +1000

    add typing sequence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
-import { HeartIcon } from "@heroicons/react/24/solid";
+import React from "react";
 import { AboutMeApp } from "./components/AboutMeApp";
 import { ByMeApp } from "./components/ByMeApp";
 import { ForFunApp } from "./components/ForFunApp";
 import { ContactMeApp } from "./components/ContactMeApp";
+import { TypeAnimation } from "react-type-animation";
+import GraphemeSplitter from "grapheme-splitter";
 
 export default function Home() {
+  const splitter = new GraphemeSplitter();
+  const ref = React.createRef<HTMLSpanElement>();
+
+  const CURSOR_CLASS_NAME = "custom-type-animation-cursor";
+
+  const showCursorAnimation = (show: boolean) => {
+    if (!ref.current) {
+      return;
+    }
+
+    const el = ref.current;
+    if (show) {
+      el.classList.add(CURSOR_CLASS_NAME);
+    } else {
+      el.classList.remove(CURSOR_CLASS_NAME);
+    }
+  };
+
   return (
     <div>
       <div className="absolute w-70 h-70  translate-x-[50px] translate-y-[-220px] md:w-100 md:h-100 md:translate-x-[100px] md:translate-y-[-250px]  bg-mypurple rounded-full"></div>
@@ -17,9 +37,25 @@ export default function Home() {
       <div className="flex flex-col w-screen h-screen justify-center items-center gap-10 pt-10 md:gap-15 md:pt-0">
         <div className="flex gap-3 md:gap-5">
           <h3 className="font-bold text-4xl md:text-6xl text-mypurple">
-            Hi! I&apos;m Angel
+            <TypeAnimation
+              ref={ref}
+              splitter={(str) => splitter.splitGraphemes(str)}
+              cursor={false}
+              sequence={[
+                () => showCursorAnimation(true),
+                1000,
+                "Hi there!",
+                2000,
+                "I'm Angel :)",
+                5000,
+                () => showCursorAnimation(false),
+              ]}
+              wrapper="span"
+              speed={5}
+              repeat={0}
+              style={{ display: "inline-block" }}
+            />
           </h3>
-          <HeartIcon className="size-10 md:size-15 text-mypurple" />
         </div>
 
         <div className="grid grid-cols-2 gap-7 md:flex  md:flex-row md:gap-13">
